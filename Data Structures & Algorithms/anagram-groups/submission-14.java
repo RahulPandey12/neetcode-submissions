848e9a7cdb219f9b9cc8b6a8b25d20df40fc4129
RahulPandey12 <46984519+RahulPandey12@users.noreply.github.com>
2026-05-30 22:11:18 +0530

    Add: anagram-groups - submission-14

---
+++ Data Structures & Algorithms/anagram-groups/submission-14.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+       Map<String,List<String>> hmap = new HashMap<>();
+       List<List<String>> blist= new ArrayList<>();
+       for(int i=0;i<strs.length;i++){
+        char ch[]=strs[i].toCharArray();
+        Arrays.sort(ch);
+        String key=Arrays.toString(ch);
+       
+        if(!hmap.containsKey(key)){
+            hmap.put(key,new ArrayList<>());
+        }
+        hmap.get(key).add(strs[i]);
+       }
+       for( Map.Entry<String,List<String>> entry:hmap.entrySet()){
+        blist.add(entry.getValue());
+       }
+       return blist;
+    }
+    
+}
