047e52f8bd3fcee6cc6b992a1359e453551de22a
RahulPandey12 <46984519+RahulPandey12@users.noreply.github.com>
2026-07-05 00:31:21 +0530

    Add: longest-consecutive-sequence - submission-17

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-17.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int longestConsecutive(int[] nums) {
+      if(nums.length==0){
+         return 0;
+      }
+    Arrays.sort(nums);
+    int maxLength=0;
+    int currentLength=1;
+    for(int i=1;i<nums.length;i++){
+
+      if((nums[i]-nums[i-1])==1){
+         currentLength++;
+      }
+      else if(nums[i]==nums[i-1]){
+         continue;
+      }
+      else{
+         maxLength=Integer.max(currentLength,maxLength);
+         currentLength=1;
+      }
+    }
+    maxLength=Integer.max(currentLength,maxLength);
+    return maxLength;
+    }
+}
