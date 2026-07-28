cd2f1324e8abdafcce7629529aa13e9d5cde5075
RahulPandey12 <46984519+RahulPandey12@users.noreply.github.com>
2026-07-29 00:51:47 +0530

    Add: longest-repeating-substring-with-replacement - submission-11

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-11.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int characterReplacement(String s, int k) {
+        int i=0;
+        int j=0;
+        HashMap<Character,Integer> hmap= new HashMap<>();
+        int maxLength=0;
+        while(j<s.length()){
+         hmap.put(s.charAt(j),hmap.getOrDefault(s.charAt(j),0)+1);
+         int maxFreq=0;
+         for(int value:hmap.values()){
+            maxFreq=Math.max(maxFreq,value);
+         }
+            if(j-i+1-maxFreq<=k){
+                maxLength=Math.max(j-i+1,maxLength);
+               
+            }
+           if(j-i+1-maxFreq>k){
+                 hmap.put(s.charAt(i),hmap.get(s.charAt(i))-1);
+                 if(hmap.get(s.charAt(i))==0){
+                    hmap.remove(s.charAt(i));
+                   
+                 }
+                  i++;
+                  maxFreq = 0;
+                for (int value : hmap.values()) {
+                    maxFreq = Math.max(maxFreq, value);
+                }
+            }
+            j++;
+        }
+        return maxLength;
+    }
+}
