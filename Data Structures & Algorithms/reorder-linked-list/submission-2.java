ef5c01ff07a954b0e89ceec01ec3c9a249c43f6c
RahulPandey12 <46984519+RahulPandey12@users.noreply.github.com>
2026-09-23 13:21:29 +0530

    Add: reorder-linked-list - submission-2

---
+++ Data Structures & Algorithms/reorder-linked-list/submission-2.java
@@ -0,0 +1,70 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Solution {
+    public ListNode reorderList(ListNode head) {
+      
+        ListNode middle=findMiddle(head);
+        ListNode reverse=reverseList(middle.next);
+        middle.next=null; 
+        ListNode temp=head;
+        ListNode temp1=reverse;
+        ListNode dummy=new ListNode(-1);
+        ListNode current=dummy;
+        
+        while(temp!=null && temp1!=null){
+            ListNode next= temp.next;
+            ListNode next1= temp1.next;
+            current.next = temp;
+            current = current.next;
+            current.next = temp1;
+            current = current.next;
+            temp = next;
+            temp1 = next1;
+        }
+          if(temp != null) {
+    current.next = temp;
+          }
+  
+       return dummy.next;
+       
+    }
+    public ListNode findMiddle(ListNode head) {
+
+    ListNode slow = head;
+    ListNode fast = head;
+
+    while(fast != null && fast.next != null) {
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+
+    return slow;
+}
+public ListNode reverseList(ListNode head) {
+
+    ListNode prev = null;
+    ListNode current = head;
+
+    while(current != null) {
+
+        ListNode next = current.next;
+
+        current.next = prev;
+
+        prev = current;
+        current = next;
+    }
+    
+
+    return prev;
+}
+}
